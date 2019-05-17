a42615826ad8bc52d635624434fac28090a69b34
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-05-17 16:30:14 -0700

    Merge master

--- dist/nearlib.js
+++ dist/nearlib.js
@@ -15263,12 +15263,11 @@ class WalletAccessKey {
 module.exports = WalletAccessKey;
 
 },{"./signing/browser_local_storage_key_store":69,"./signing/key_pair":71,"./signing/simple_key_store_signer":72}],74:[function(require,module,exports){
-(function (Buffer){
 /**
  * Wallet based account and signer that uses external wallet through the iframe to sign transactions.
  */
 
-const { FunctionCallTransaction } = require('./protos');
+const { BrowserLocalStorageKeystore, KeyPair, SimpleKeyStoreSigner } = require('.');
 
 const LOGIN_WALLET_URL_SUFFIX = '/login/';
 
@@ -15287,7 +15286,7 @@ const PENDING_ACCESS_KEY_PREFIX = 'pending_key'; // browser storage key for a pe
  */
 class WalletAccount {
 
-    constructor(appKeyPrefix, walletBaseUrl = 'https://wallet.nearprotocol.com', keyStore = new nearlib.BrowserLocalStorageKeystore()) {
+    constructor(appKeyPrefix, walletBaseUrl = 'https://wallet.nearprotocol.com', keyStore = new BrowserLocalStorageKeystore()) {
         this._walletBaseUrl =  walletBaseUrl;
         this._authDataKey = appKeyPrefix + LOCAL_STORAGE_KEY_SUFFIX;
         this._keyStore = keyStore;
@@ -15339,7 +15338,7 @@ class WalletAccount {
         newUrl.searchParams.set('failure_url', failure_url || currentUrl.href);
         newUrl.searchParams.set('app_url', currentUrl.origin);
         if (!this.getAccountId() || !this._keyStore.getKey(this.getAccountId())) {
-            const accessKey = nearlib.KeyPair.fromRandomSeed();
+            const accessKey = KeyPair.fromRandomSeed();
             newUrl.searchParams.set('public_key', accessKey.getPublicKey());
             this._keyStore.setKey(PENDING_ACCESS_KEY_PREFIX + accessKey.getPublicKey(), accessKey).then(window.location.replace(newUrl.toString()));
         }
@@ -15387,12 +15386,8 @@ class WalletAccount {
         if (!this.isSignedIn() || originator !== this.getAccountId()) {
             throw 'Unauthorized account_id ' + originator;
         }
-        const body = FunctionCallTransaction.decode(buffer);
-        let methodName = Buffer.from(body.methodName).toString();
-        let args = JSON.parse(Buffer.from(body.args).toString());
-        const signer = new nearlib.SimpleKeyStoreSigner(this._keyStore);
+        const signer = new SimpleKeyStoreSigner(this._keyStore);
         let signature = await signer.signBuffer(buffer, originator);
-        console.log(signature);
         return signature;
     }
 
@@ -15400,5 +15395,4 @@ class WalletAccount {
 
 module.exports = WalletAccount;
 
-}).call(this,require("buffer").Buffer)
-},{"./protos":67,"buffer":21}]},{},[2]);
+},{".":3}]},{},[2]);
